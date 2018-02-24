959a5bbac4711ddc7b62abce03e22fc091070bd9
im100ruv <karnasaurabh123@gmail.com>
2018-02-24 10:58:03 +0530

    initial commit with map to yiit

--- MapDemo/ViewController.swift
+++ MapDemo/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    // Outlets
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let latitude: CLLocationDegrees = 13.6558231
+        let longitude: CLLocationDegrees = 79.4369599
+        let latDelta: CLLocationDegrees = 0.02
+        let lonDelta: CLLocationDegrees = 0.02
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+        let coordinates = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        let region = MKCoordinateRegion(center: coordinates, span: span)
+        
+        mapView.setRegion(region, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
